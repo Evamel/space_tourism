eb12b2374a0108f2e68ed557007d99b12a524188
Eva Mélinand <eva.melinand@gmail.com>
2021-12-13 16:19:40 +0100

    link js and html to import header ok + style link ok

---
+++ nav.js
@@ -0,0 +1,7 @@
+fetch("../elements/nav.html")
+    .then(response => {
+        return response.text();
+    })
+    .then(data => {
+        document.querySelector("header").innerHTML = data;
+    });
